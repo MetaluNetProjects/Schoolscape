d242378866fea180d35e64e08055b5b51e14d355
Antoine Rousseau <_antoine_@metalu.net>
2020-10-08 14:55:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Schoolscape
+## House music app for school
+
+------
+**Schoolscape** is a mobile app offering to play 
+electronic music with sounds you can record yourself.  
+These sounds should be recorded from various household items, 
+such as your washing machine or microwave oven.
+
+------
+
+**Schoolscape** is written with [Pure Data](http://pure-data.info), 
+and uses [Pof](https://github.com/Ant1r/ofxPof) to build the GUI.  
+It's turned to a mobile app with the help of [ofxPd](https://github.com/danomatika/ofxPd), 
+itself thanks to [libpd](http://libpd.cc) and [openFrameworks](http://www.openframeworks.cc).
+
+------
+
+GNU GENERAL PUBLIC LICENSE  
+[Al1&Ant1](http://al1ant1.free.fr/)  
+[Lieu Multiple](https://lieumultiple.org) & [metalu.net](http://metalu.net/en/home/)
+
+------
+wiki in english :
+[Schoolscape(en)](https://github.com/MetaluNetProjects/Schoolscape/wiki/Schoolscape(en))
+Wiki en français :
+[Schoolscape(fr)](http://linuxmao.org/Schoolscape)
+
+------
---
+++ Schoolscape-android/src/testApp.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014 Antoine Rousseau <antoine@metalu.net>
+ * BSD Simplified License, see the file "LICENSE.txt" in this distribution.
+ * See https://github.com/Ant1r/ofxPof for documentation and updates.
+ */
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAndroid.h"
+#include "ofxPd.h"
+#include "opensl_stream/opensl_stream.h"
+
+// a namespace for the Pd types
+using namespace pd;
+
+class testApp : public ofxAndroidApp, public PdReceiver {
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+		void exit();
+		void reloadTextures();
+		void unloadTextures();
+		
+		void keyPressed  (int key);
+		virtual void touchDown(int x, int y, int id);
+		virtual void touchMoved(int x, int y, int id);
+		virtual void touchUp(int x, int y, int id);
+		virtual void touchDoubleTap(int x, int y, int id) {};
+		virtual void touchCancelled(int x, int y, int id) {};
+		virtual bool backPressed();/*{
+			ofLogNotice("testApp", "back pressed");
+			exit();
+			ofExit();
+			return true;
+		}*/
+
+		void windowResized(int w, int h);
+		
+		void audioReceived(float * input, int bufferSize, int nChannels);
+		void audioRequested(float * output, int bufferSize, int nChannels);
+		
+		// pd message receiver callbacks
+		void print(const std::string& message);
+		
+		void receiveBang(const std::string& dest);
+		void receiveFloat(const std::string& dest, float value);
+		void receiveSymbol(const std::string& dest, const std::string& symbol);
+		void receiveList(const std::string& dest, const List& list);
+		void receiveMessage(const std::string& dest, const std::string& msg, const List& list);
+
+		OPENSL_STREAM *os;
+		ofxPd puda;
+		static void opensl_process(void *app, int sample_rate, int buffer_frames,
+			int input_channels, const short *input_buffer,
+			int output_channels, short *output_buffer);
+
+};
